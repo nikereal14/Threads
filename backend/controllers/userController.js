8d56f444df56ea75686c7dfebbc081b7591f38a6
nikereal14 <dirty.cursed@gmail.com>
2023-10-17 19:24:53 +0300

    made a registration on the backend, saving the JWT token in cookies

---
+++ backend/controllers/userController.js
@@ -0,0 +1,42 @@
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
+import generateTokenAndSetCookie from '../utils/helpers/generateTokenAndSetCookie.js';
+
+const signupUser = async (req, res) => {
+	try {
+		const { name, email, username, password } = req.body;
+		const user = await User.findOne({ $or: [{ email }, { username }] });
+
+		if (user) {
+			return res.status(400).json({ message: 'User already exists' });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			name,
+			email,
+			username,
+			password: hashedPassword,
+		});
+		await newUser.save();
+
+		if (newUser) {
+			generateTokenAndSetCookie(newUser._id, res);
+			res.status(201).json({
+				id: newUser._id,
+				name: newUser.name,
+				email: newUser.email,
+				username: newUser.username,
+			});
+		} else {
+			res.status(400).json({ message: 'Invalid user data' });
+		}
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+		console.log('Error in signupUser: ', err.message);
+	}
+};
+
+export { signupUser };
