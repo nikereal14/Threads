5ca873b31cdcc351a00c541292b6077b715a9cc8
nikereal14 <dirty.cursed@gmail.com>
2023-10-20 00:18:03 +0300

    made follow/unfollow✅

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -2,12 +2,23 @@ import { Avatar } from '@chakra-ui/avatar';
 import { Box, Flex, Link, Text, VStack } from '@chakra-ui/layout';
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu';
 import { Portal } from '@chakra-ui/portal';
-import { useToast } from '@chakra-ui/react';
+import { Button, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
 import { BsInstagram } from 'react-icons/bs';
 import { CgMoreO } from 'react-icons/cg';
+import { Link as RouterLink } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import userAtom from '../atoms/userAtom';
+import useShowToast from '../hooks/useShowToast';
 
-const UserHeader = () => {
+const UserHeader = ({ user }) => {
 	const toast = useToast();
+	const currentUser = useRecoilValue(userAtom);
+	const showToast = useShowToast();
+	const [following, setFollowing] = useState(
+		user.followers.includes(currentUser._id)
+	);
+	const [updating, setUpdating] = useState(false);
 
 	const copyURL = () => {
 		const currentURL = window.location.href;
@@ -21,15 +32,53 @@ const UserHeader = () => {
 			});
 		});
 	};
+
+	const handleFollowUnfollow = async () => {
+		if (!currentUser) {
+			showToast('Error', 'Please login to follow', 'error');
+			return;
+		}
+		if (updating) return;
+		setUpdating(true);
+		try {
+			const res = await fetch(`/api/users/follow/${user._id}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			const data = await res.json();
+			if (data.error) {
+				showToast('Error', data.error, 'error');
+				return;
+			}
+
+			if (following) {
+				showToast('Success', `Unfollowed ${user.name}`, 'success');
+				user.followers.pop();
+			} else {
+				showToast('Success', `Followed ${user.name}`, 'success');
+				user.followers.push(currentUser._id);
+			}
+			setFollowing(!following);
+
+			console.log(data);
+		} catch (error) {
+			showToast('Error', error, 'error');
+		} finally {
+			setUpdating(false);
+		}
+	};
+
 	return (
 		<VStack gap={4} alignItems={'start'}>
 			<Flex justifyContent={'space-between'} w={'full'}>
 				<Box>
 					<Text fontSize={'2xl'} fontWeight={'bold'}>
-						Dmitry Tarasov
+						{user.name}
 					</Text>
 					<Flex gap={2} alignItems={'center'}>
-						<Text fontSize={'sm'}>dekaien</Text>
+						<Text fontSize={'sm'}>{user.username}</Text>
 						<Text
 							fontSize={'xs'}
 							bg={'gray.dark'}
@@ -42,21 +91,44 @@ const UserHeader = () => {
 					</Flex>
 				</Box>
 				<Box>
-					<Avatar
-						name='Dmitry Tarasov'
-						src='/zuck-avatar.png'
-						size={{
-							base: 'md',
-							md: 'xl',
-						}}
-					/>
+					{user.profilePic && (
+						<Avatar
+							name={user.name}
+							src={user.profilePic}
+							size={{
+								base: 'md',
+								md: 'xl',
+							}}
+						/>
+					)}
+					{!user.profilePic && (
+						<Avatar
+							name={user.name}
+							src='https://bit.ly/broken-link'
+							size={{
+								base: 'md',
+								md: 'xl',
+							}}
+						/>
+					)}
 				</Box>
 			</Flex>
 
-			<Text>React Javascript Programmer</Text>
+			<Text>{user.bio}</Text>
+
+			{currentUser._id === user._id && (
+				<Link as={RouterLink} to='/update'>
+					<Button size={'sm'}>Update Profile</Button>
+				</Link>
+			)}
+			{currentUser._id !== user._id && (
+				<Button size={'sm'} onClick={handleFollowUnfollow} isLoading={updating}>
+					{following ? 'Unfollow' : 'Follow'}
+				</Button>
+			)}
 			<Flex w={'full'} justifyContent={'space-between'}>
 				<Flex gap={2} alignItems={'center'}>
-					<Text color={'gray.light'}>3.2K followers</Text>
+					<Text color={'gray.light'}>{user.followers.length} followers</Text>
 					<Box w='1' h='1' bg={'gray.light'} borderRadius={'full'}></Box>
 					<Link color={'gray.light'}>instagram.com</Link>
 				</Flex>
--- src/pages/UpdateProfilePage.jsx
+++ src/pages/UpdateProfilePage.jsx
@@ -26,12 +26,15 @@ export default function UpdateProfilePage() {
 		password: '',
 	});
 	const fileRef = useRef(null);
+	const [updating, setUpdating] = useState(false);
 
 	const showToast = useShowToast();
 
 	const { handleImageChange, imgUrl } = usePreviewImg();
 	const handleSubmit = async e => {
 		e.preventDefault();
+		if (updating) return;
+		setUpdating(true);
 		try {
 			const res = await fetch(`/api/users/update/${user._id}`, {
 				method: 'PUT',
@@ -50,6 +53,8 @@ export default function UpdateProfilePage() {
 			localStorage.setItem('user-threads', JSON.stringify(data));
 		} catch (error) {
 			showToast('Error', error, 'error');
+		} finally {
+			setUpdating(false);
 		}
 	};
 
@@ -164,6 +169,7 @@ export default function UpdateProfilePage() {
 								bg: 'green.500',
 							}}
 							type='submit'
+							isLoading={updating}
 						>
 							Submit
 						</Button>
